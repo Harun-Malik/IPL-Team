f0fa7ecc57a89d79d39eaa4be699a472120a2989
Harun-Malik <100518628+Harun-Malik@users.noreply.github.com>
2022-08-01 15:25:16 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,51 @@
+// let mi = document.getElementById('mi')
+// let rr = document.getElementById('rr')
+// let csk = document.getElementById('csk')
+// let dc = document.getElementById('dc')
+// let gt = document.getElementById('gt')
+// let kkr = document.getElementById('kkr')
+// let lsg = document.getElementById('lsg')
+// let pbks = document.getElementById('pbks')
+// let rcb = document.getElementById('rcb')
+// let srh = document.getElementById('srh')
+// let btn = document.getElementsByClassName('btn')
+// console.log(mi)
+
+// // console.log(mi,rr,csk,dc,gt,kkr,lsg,pbks,rcb,srh,btn)
+// btn.onclick = function(){
+//     console.log('harun')
+// }
+
+// mi.addEventListener('click', ()=>{
+//     // location.search = '1-mi.html'
+//     console.log('harun')
+
+// })
+  
+// rr.onclick = ()=>{
+//     location.search = '2-rr.html'
+// }
+// csk.onclick = ()=>{
+//     location.search = '3-csk.html'
+// }
+// dc.onclick = ()=>{
+//     location.search = '4-dc.html'
+// }
+// gt.onclick = ()=>{
+//     location.search = '5-gt.html'
+// }
+// kkr.onclick = ()=>{
+//     location.search = '6-kkr.html'
+// }
+// lsg.onclick = ()=>{
+//     location.search = '7-lsg.html'
+// }
+// pbks.onclick = ()=>{
+//     location.search = '8-pbks.html'
+// }
+// rcb.onclick = ()=>{
+//     location.search = '9-rcb.html'
+// }
+// srh.onclick = ()=>{
+//     location.search = '10-srh.html'
+// }
